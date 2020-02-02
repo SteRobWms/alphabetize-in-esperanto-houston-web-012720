d07b75c8ea1d8b4cb5257a51ac97198c73c3b371
SteRobWms <stephen.r.williams.87@gmail.com>
2020-02-01 21:01:51 -0600

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
-def alphabetize(arr)
-  # code here
+require 'pry'
+
+def alphabetize(array)
+  alphabet = 'abcĉdefgĝhĥijĵklmnoprsŝtuŭvz'
+  
+  #found online, modified to use "chars" instead of split('') because i think it reads clearer
+  #starts sort
+  array.sort_by do |phrase|
+    #split phrase into array of characters, then starts map
+    phrase.chars.map do |letter|
+      #each letter in array is substituted for the index of the letter, using the provided alphabet
+      alphabet.index(letter)
+    end
+    #the returned arrays of numbers are used to sort the original phrase
+  end
 end
